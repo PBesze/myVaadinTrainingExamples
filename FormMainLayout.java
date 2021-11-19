7a4d88e846c07eec9fd94756cdcd222aef1033c1
Peter Besze <pbesze@gmail.com>
2021-11-19 11:47:47 +0100

    Update FormMainLayout.java

--- FormMainLayout.java
+++ FormMainLayout.java
@@ -115,7 +115,7 @@ public class FormMainLayout extends VerticalLayout {
 			    deliveryInButton.addClickListener(e ->
 			    	{	     
 			    	deliveryInButton.getUI().ifPresent(ui ->  ui.navigate("del"));
-			    	 UI.getCurrent().getPage().reload(); //if I don't use this, the content DIV will not refreshed
+			    	 UI.getCurrent().getPage().reload(); //if I don't use this, the content DIV will not refreshed. How can I "refresh just the DIV"?
 			    	});
 				}
 			
